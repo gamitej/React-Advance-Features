969f4cdfba9795256d156d6a0bd0a07bd9505048
gamitej <gamitejpratapsingh998@gmail.com>
2023-07-26 17:50:30 +0530

    tree structure done

---
+++ src/DummyData/data-set-1.js
@@ -0,0 +1,75 @@
+export const stateData = [
+  {
+    id: "1",
+    state: "Madhya Pradesh",
+    children: [
+      {
+        id: "2",
+        city: "Gwalior",
+        children: [
+          {
+            id: "3",
+            village: "Badwari",
+          },
+          { id: "4", village: "Rora" },
+        ],
+      },
+      {
+        id: "5",
+        city: "Morena",
+        children: [
+          {
+            id: "6",
+            village: "Nayakpura",
+          },
+          {
+            id: "7",
+            village: "Jigni",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 8,
+    state: "Bihar",
+    children: [
+      {
+        id: 9,
+        city: "Patna",
+        villages: [
+          {
+            id: 10,
+            village: "Fatuha",
+          },
+          {
+            id: 11,
+            village: "Dulhinbazar",
+          },
+          {
+            id: 12,
+            village: "Bihata",
+          },
+          {
+            id: 13,
+            village: "Danapur",
+          },
+        ],
+      },
+      {
+        id: 14,
+        city: "Gaya",
+        villages: [
+          {
+            id: 15,
+            village: "Amraha",
+          },
+          {
+            id: 16,
+            village: "Belthu",
+          },
+        ],
+      },
+    ],
+  },
+];
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,38 @@
+import { stateData } from "../../DummyData/data-set-1";
+import { Tree } from "react-arborist";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="">
+      <Tree
+        initialData={stateData}
+        openByDefault={false}
+        width={600}
+        height={1000}
+        indent={24}
+        rowHeight={36}
+        paddingTop={30}
+        paddingBottom={10}
+        padding={25 /* sets both */}
+      >
+        {Node}
+      </Tree>
+    </div>
+  );
 };
+
+function Node({ node, style, dragHandle }) {
+  /* This node instance can do many things. See the API reference. */
+  return (
+    <div style={style} ref={dragHandle} onClick={() => node.toggle()}>
+      {node.isLeaf ? "🍁" : ">"}{" "}
+      {node.data.state !== undefined
+        ? node.data.state
+        : node.data.city !== undefined
+        ? node.data.city
+        : node.data.village}
+    </div>
+  );
+}
 
 export default Home;
