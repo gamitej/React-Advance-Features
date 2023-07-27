72c31f27fd5d04d35edba8e960a65803be2ac799
gamitej <gamitejpratapsingh998@gmail.com>
2023-07-27 16:40:40 +0530

    filter in progress

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // libs
 import { orderBy } from "lodash";
 // comp
@@ -20,9 +20,34 @@ const searchList = [
 ];
 
 const Home = () => {
+  const [type, setType] = useState({ poor: true, rich: true });
+
+  const handleType = (val) => {
+    setType((prevState) => ({ ...prevState, [val]: !prevState[val] }));
+  };
+
   return (
     <div>
-      <SearchBar searchList={searchList} />
+      <div className="flex w-[18rem] justify-around  items-center h-[5rem]">
+        <button
+          className={`${
+            type.poor ? "bg-red-100" : "border-2 border-red-300 bg-white"
+          } p-2 w-[7rem] rounded-md h-[3rem]`}
+          onClick={() => handleType("poor")}
+        >
+          poor
+        </button>
+        <button
+          className={`${
+            type.rich ? "bg-green-100" : "border-2 border-green-300 bg-white"
+          } p-2 w-[7rem] rounded-md h-[3rem]`}
+          onClick={() => handleType("rich")}
+        >
+          rich
+        </button>
+      </div>
+
+      {/* <SearchBar searchList={searchList} /> */}
       <TreeView stateData={stateData} />
     </div>
   );
