7cd03e90f9e018c0ad37f028cae8841c6d5ccc14
gamitej <gamitejpratapsingh998@gmail.com>
2023-07-27 07:39:48 +0530

    re structured

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,67 +1,12 @@
-import { stateData } from "../../DummyData/data-set-1";
-import { Tree } from "react-arborist";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import CabinIcon from "@mui/icons-material/Cabin";
+import React from "react";
+import TreeView from "../Tree/TreeView";
 
 const Home = () => {
   return (
-    <div className="">
-      <Tree
-        initialData={stateData}
-        openByDefault={false}
-        width={600}
-        height={1000}
-        indent={24}
-        rowHeight={45}
-        paddingTop={30}
-        paddingBottom={10}
-        padding={25 /* sets both */}
-      >
-        {Node}
-      </Tree>
+    <div>
+      <TreeView />
     </div>
   );
 };
-
-function Node({ node, style, dragHandle }) {
-  const nodeData = node.data;
-  const tree = node.tree;
-  const treeIsOpen = tree.isOpen(nodeData.id);
-
-  return (
-    <div style={style} ref={dragHandle} onClick={() => node.toggle()}>
-      {node.isLeaf && (
-        <div className="cursor-pointer bg-slate-100 p-2">
-          {" "}
-          <CabinIcon /> {nodeData.village}
-        </div>
-      )}
-      {!node.isLeaf && (
-        <div>
-          {nodeData.state !== undefined && (
-            <div className="cursor-pointer bg-blue-100 p-2">
-              <span>
-                <KeyboardArrowRightIcon
-                  className={`${treeIsOpen ? "rotate-90" : ""}`}
-                />
-              </span>
-              {nodeData.state}
-            </div>
-          )}
-          {nodeData.state === undefined && (
-            <div className="cursor-pointer bg-slate-200 p-2">
-              <span>
-                <KeyboardArrowRightIcon
-                  className={`${treeIsOpen ? "rotate-90" : ""}`}
-                />
-              </span>{" "}
-              {nodeData.city}
-            </div>
-          )}
-        </div>
-      )}
-    </div>
-  );
-}
 
 export default Home;
