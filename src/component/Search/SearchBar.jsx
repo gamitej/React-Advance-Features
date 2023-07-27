cb4f996a875938caa4c230b9365d2d70c93ca796
gamitej <gamitejpratapsingh998@gmail.com>
2023-07-27 12:21:54 +0530

    search in-progress

--- src/component/Search/SearchBar.jsx
+++ src/component/Search/SearchBar.jsx
@@ -1,21 +1,25 @@
 import React, { useMemo, useState } from "react";
 
-const SearchBar = ({ searchList = [] }) => {
+const SearchBar = ({ searchList = [], inputFieldFontSize = "1rem" }) => {
   const [inputValue, setInputValue] = useState("");
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
   const handleInputChange = (e) => {
     const value = e.target.value;
     setInputValue(value);
-
     setIsDropdownVisible(value !== "");
   };
 
+  // ========= filter search-list as per input  ========
   const filteredSearchList = useMemo(() => {
     return searchList?.filter((item) =>
       item.toLowerCase().includes(inputValue.toLowerCase())
     );
   }, [inputValue]);
+
+  /**
+   * JSX
+   */
 
   return (
     <div className="relative z-[1000]">
@@ -23,6 +27,7 @@ const SearchBar = ({ searchList = [] }) => {
         type="text"
         placeholder="Enter your input ..."
         className={`border border-slate-300 p-3 rounded-xl w-[20rem]`}
+        style={{ fontSize: inputFieldFontSize }}
         value={inputValue}
         onChange={handleInputChange}
       />
